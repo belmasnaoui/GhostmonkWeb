86f5978c15a996c04686dd1a4de81889d42076e9
Nicholas Hillier <nicholas@ghostmonk.com>
2011-03-29 18:25:37 -0400

    Yikes, Linq2Twitter errors out pretty easy. This is a quick hack until I find something better.

--- GhostmonkMainSite/Controllers/HomeController.cs
+++ GhostmonkMainSite/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
-using GhostmonkMainSite.Utils;
 using LinqToTwitter;
 
 namespace GhostmonkMainSite.Controllers
 {
     public class HomeController : Controller
     {
+        private const string FAIL_WHALE = "Oooops! communication with Twitter failed...";
+
         public ActionResult Index()
         {
             return View();
@@ -16,13 +17,21 @@ namespace GhostmonkMainSite.Controllers
         public PartialViewResult TwitterFeed()
         {
             TwitterContext twitter = new TwitterContext();
+            Status target;
+            try
+            {
+                target = ( from tweet in twitter.Status
+                                  where tweet.Type == StatusType.User &&
+                                        tweet.ScreenName == "ghostmonk" &&
+                                        tweet.IncludeRetweets == true &&
+                                        tweet.Count == 1
+                                  select tweet ).First();
+            }
+            catch
+            {
+                target = new Status { Text = FAIL_WHALE };
+            }
 
-            Status target = ( from  tweet in twitter.Status
-                              where tweet.Type == StatusType.User &&
-                                    tweet.ScreenName == "ghostmonk" &&
-                                    tweet.IncludeRetweets == false &&
-                                    tweet.Count == 1
-                              select tweet ).First();
 
             return PartialView( "Twitter", target );
         }
